ba97a3441907380749522df0546119a882bbcbea
overtrue <anzhengchao@gmail.com>
2018-05-27 17:41:19 +0800

    加大导航字号

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-white navbar-light shadow-6 d-flex justify-content-between align-items-center">
+  <nav class="navbar text-16 navbar-expand-lg bg-white navbar-light shadow-6 d-flex justify-content-between align-items-center">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
