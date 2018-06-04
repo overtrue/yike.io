e25f287896a0d022a530d185c13a6b026310cfe7
jcc <changejian@gmail.com>
2018-06-04 16:30:35 +0800

    添加页面访问权限

--- src/modules/notifications/routes.js
+++ src/modules/notifications/routes.js
@@ -4,22 +4,37 @@ export default [{
   children: [{
     path: '/',
     name: 'notifications.all',
+    meta: {
+      requiresAuth: true,
+    },
     component: () => import('./classifications/all')
   }, {
     path: 'mention',
     name: 'notifications.mention',
+    meta: {
+      requiresAuth: true,
+    },
     component: () => import('./classifications/mention')
   }, {
     path: 'reply',
     name: 'notifications.reply',
+    meta: {
+      requiresAuth: true,
+    },
     component: () => import('./classifications/reply')
   }, {
     path: 'follower',
     name: 'notifications.follower',
+    meta: {
+      requiresAuth: true,
+    },
     component: () => import('./classifications/follower')
   }, {
     path: 'letter',
     name: 'notifications.letter',
+    meta: {
+      requiresAuth: true,
+    },
     component: () => import('./classifications/letter')
   }]
 }]
--- src/modules/users/routes.js
+++ src/modules/users/routes.js
@@ -36,22 +36,37 @@ export default [{
   children: [{
     path: 'profile',
     name: 'user.profile',
+    meta: {
+      requiresAuth: true,
+    },
     component: () => import('./profile/basic')
   }, {
     path: 'account',
     name: 'user.account',
+    meta: {
+      requiresAuth: true,
+    },
     component: () => import('./profile/account')
   }, {
     path: 'notification',
     name: 'user.notification-setting',
+    meta: {
+      requiresAuth: true,
+    },
     component: () => import('./profile/notification')
   }, {
     path: 'bindings',
     name: 'user.bindings',
+    meta: {
+      requiresAuth: true,
+    },
     component: () => import('./profile/bindings')
   }, {
     path: 'blacklist',
     name: 'user.blacklist',
+    meta: {
+      requiresAuth: true,
+    },
     component: () => import('./profile/blacklist')
   }]
 }]
