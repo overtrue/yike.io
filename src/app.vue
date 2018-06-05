09db9787a267037bfae1dda96b08cb65902b2269
jcc <changejian@gmail.com>
2018-06-05 22:26:29 +0800

    账号邮箱激活

--- src/app.vue
+++ src/app.vue
@@ -1,9 +1,17 @@
 <template>
   <div id="app">
     <Navbar />
-    <div class="alert alert-warning text-center m-0" role="alert" v-if="!currentUser.has_activated">
-      您的邮箱尚未验证，以免影响你的使用，请先<a href="#" class="text-blue">验证邮箱</a>！
+    <div class="alert alert-warning text-center m-0" role="alert" v-if="currentUser.id && !currentUser.has_activated">
+      您的邮箱尚未验证，将影响你的正常使用。请先验证您的邮箱，如果未收到邮件，请点击<a href="javascript:void(0);" class="text-blue" @click="sendActiveMail">重新发送</a>！
     </div>
+    <template v-if="$route.query['active-success']">
+      <div class="alert alert-success text-center m-0" role="alert" v-if="$route.query['active-success'] == 'yes'">
+        您的账号已激活！
+      </div>
+      <div class="alert alert-danger text-center m-0" role="alert" v-else>
+        邮箱验证失败，请<a href="javascript:void(0);" class="text-blue" @click="sendActiveMail">重新发送验证邮件</a>！
+      </div>
+    </template>
     <div class="main-content" :class="{'container my-3': withContainer}">
       <router-view />
     </div>
@@ -27,6 +35,13 @@
         }
 
         return true
+      }
+    },
+    methods: {
+      sendActiveMail() {
+        this.api('user/send-active-mail').post().then((data) => {
+          this.$message.success(data.message)
+        })
       }
     }
   }
