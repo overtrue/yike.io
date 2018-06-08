37f564fec53bfc15fb5dab9edce34a731e569d6e
overtrue <anzhengchao@gmail.com>
2018-06-08 11:40:15 +0800

    修复粉丝列表

---
+++ src/components/user-list-item.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="user-media d-flex">
+    <router-link :to="{name: 'users.show', params:{id: user.id}}">
+      <img :src="user.avatar" class="avatar-40" :alt="user.name"/>
+    </router-link>
+    <div class="ml-2">
+      <div>
+        <router-link tag="a" class="d-inline-block" :to="{name: 'users.show', params: {id: user.id}}">
+          <slot name="name"><h5 class="mb-0 text-gray-50" :class="nameClasses">{{ user.name }}</h5></slot>
+        </router-link>
+        <slot name="name-appends">
+          <router-link tag="a" class="ml-1 text-muted text-12 d-inline-block" :to="{name: 'users.show', params: {id: user.id}}">
+            {{ user.username }}
+          </router-link>
+        </slot>
+      </div>
+      <slot name="description">
+        <div class="text-12 text-gray-70">{{ user.bio }}</div>
+      </slot>
+    </div>
+    <slot name="appends" :data="user">
+      <follow-btn :user="user" :simple="true" class="ml-auto"></follow-btn>
+    </slot>
+  </div>
+</template>
+
+<script>
+  import FollowBtn from '@components/follow-btn'
+  export default {
+    name: 'user-list-item',
+    components: {FollowBtn},
+    props: {
+      user: {
+        type: Object
+      },
+    }
+  }
+</script>
