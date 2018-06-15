35501784944c4684347b8cd48447f3308c3037e1
jcc <changejian@gmail.com>
2018-06-15 23:36:04 +0800

    优化样式

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -1,15 +1,15 @@
 <template>
   <div id="sidebar-wrapper" class="border-right position-fixed h-100 bg-white">
-    <ul class="sidebar-nav">
+    <ul class="px-2">
       <li class="text-16 text-gray-40 py-2">数据统计</li>
       <li>
         <div class="text-14 font-weight-bold text-gray-40">站点统计</div>
         <ul class="text-12 px-2">
-          <li class="d-flex justify-content-between">
+          <li class="d-flex justify-content-between py-1">
             <span>总人数</span>
             <span>1000</span>
           </li>
-          <li class="d-flex justify-content-between">
+          <li class="d-flex justify-content-between py-1">
             <span>总帖子数</span>
             <span>1000</span>
           </li>
