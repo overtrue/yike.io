1c21eadd5dba60bf25ab4736b6b4d4bf5ad697f2
overtrue <anzhengchao@gmail.com>
2018-06-17 21:29:39 +0800

    个人主页添加社交按钮

---
+++ src/components/user-social-btns.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="user-social-btns d-flex justify-content-end">
+        <button type="button" :class="classes" class="btn btn-dark btn-icon">
+          <github-icon></github-icon>
+        </button>
+        <button type="button" :class="classes" class="btn btn-twitter btn-icon">
+          <twitter-icon></twitter-icon>
+        </button>
+        <button type="button" :class="classes" class="btn btn-facebook btn-icon">
+          <facebook-icon></facebook-icon>
+        </button>
+        <button type="button" :class="classes" class="btn btn-orange btn-icon text-white">
+          <instagram-icon></instagram-icon>
+        </button>
+        <button type="button" :class="classes" class="btn btn-teal-blue btn-icon text-white">
+          <telegram-icon></telegram-icon>
+        </button>
+        <button type="button" :class="classes" class="btn btn-payne-grey btn-icon text-white">
+          <steam-icon></steam-icon>
+        </button>
+    </div>
+</template>
+
+<script>
+  import TwitterIcon from '@icons/twitter'
+  import GithubIcon from '@icons/github-circle'
+  import FacebookIcon from '@icons/facebook'
+  import InstagramIcon from '@icons/instagram'
+  import TelegramIcon from '@icons/telegram'
+  import SteamIcon from '@icons/steam'
+
+  export default {
+    name: 'user-social-btns',
+    props: {
+      user: {
+        type: Object,
+        required: true
+      },
+      size: {
+        type: Number,
+        default: 16,
+      }
+    },
+    computed: {
+      classes() {
+        return 'text-'+this.size
+      }
+    },
+    components: {TwitterIcon, GithubIcon, FacebookIcon, InstagramIcon, TelegramIcon, SteamIcon},
+  }
+</script>
+
+<style scoped>
+
+</style>
