1b87acd20eed94e1d5761cfe6b418da4fe0c1ebb
overtrue <anzhengchao@gmail.com>
2018-06-29 22:05:50 +0800

    Fix vue config.

--- vue.config.js
+++ vue.config.js
@@ -24,7 +24,7 @@ module.exports = {
 
   // use the full build with in-browser compiler?
   // https://vuejs.org/v2/guide/installation.html#Runtime-Compiler-vs-Runtime-only
-  compiler: false,
+  runtimeCompiler: false,
 
   // babel-loader skips `node_modules` deps by default.
   // explicitly transpile a dependency with this option.
