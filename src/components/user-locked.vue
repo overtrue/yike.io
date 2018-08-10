6118b0e05c99d8595780e4308ed4ffc4603ba38e
overtrue <anzhengchao@gmail.com>
2018-08-10 10:22:22 +0800

    账户锁定

---
+++ src/components/user-locked.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="box text-center text-danger"><h1><account-off-icon/></h1> 该账户已被冻结</div>
+</template>
+
+<script>
+  import AccountOffIcon from '@icons/account-off'
+  export default {
+    name: 'user-locked',
+    components: {AccountOffIcon}
+  }
+</script>
