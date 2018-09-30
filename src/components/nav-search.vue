7597fdee5d2c65cce84f510f02e012ee2f72cb5d
overtrue <anzhengchao@gmail.com>
2018-09-30 22:37:24 +0800

    url

--- src/components/nav-search.vue
+++ src/components/nav-search.vue
@@ -1,8 +1,8 @@
 <template>
   <form class="search-form form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" v-model="q" @blur="clear" placeholder="搜索" aria-label="Search">
+      <input class="form-control mr-sm-2" type="search" v-model="q" placeholder="搜索" aria-label="Search">
       <div class="list-group" v-if="threads.length > 0">
-        <div class="list-group-item list-group-item-action cursor-pointer" v-for="item of threads" :key="item.id" @click="$router.push({name: 'threads.show', params:{id: item.id}});clear()">
+        <div class="list-group-item list-group-item-action cursor-pointer" v-for="item of threads" :key="item.id" @click="$router.push({name: 'threads.show', params:{id: item.id}})">
           <div class="d-flex align-items-center">
             <a href="#" class="mr-2"><img :src="item.user.avatar" alt="" class="avatar-30"></a>
             <div class="highlights text-gray-50 text-truncate" v-html="item['highlights'] && item['highlights']['title'] ? item.highlights.title[0] : item.title"></div>
@@ -31,11 +31,21 @@
         }
       }
     },
+    mounted() {
+      this.$nextTick(() => {
+        document.addEventListener('click', this.methods.onBodyClick)
+      })
+    },
+    beforeDestroy() {
+      document.removeEventListener('click', this.methods.onBodyClick)
+    },
     methods: {
-      clear() {
-        this.q = ''
-        this.threads = []
-      },
+      onBodyClick(e) {
+        let searchForm = document.querySelector('.search-form')
+        if (e.target !== searchForm) {
+          this.threads = []
+        }
+      }
       highlightContent(item) {
         return (item.highlights['content'] || []).join('...')
       },
