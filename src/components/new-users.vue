3ef3fc7549654819dc8e5c6aaa9ebd76ef3d4d8f
overtrue <anzhengchao@gmail.com>
2018-12-03 21:00:40 +0800

    Fixed cs.

--- src/components/new-users.vue
+++ src/components/new-users.vue
@@ -2,24 +2,19 @@
   <div class="box box-flush text-gray-50">
     <div class="box-body">
       <div class="d-flex align-items-center justify-content-between">
-        <div class="text-13">最新戝员</div>
+        <div class="text-13">最新用户</div>
         <!--<button class="btn btn-ghost btn-icon"><arrow-right></arrow-right></button>-->
       </div>
     </div>
     <ul class="plan-list px-2 pb-2">
-      <user-media
-        type="vertical w25"
-        v-for="item in users"
-        :key="item.id"
-        :user="item"
-      ></user-media>
+      <user-media type="vertical w25" v-for="item in users" :key="item.id" :user="item"></user-media>
     </ul>
   </div>
 </template>
 
 <script>
-import UserMedia from '@components/user-media';
-import ArrowRight from '@icons/arrow-right';
+import UserMedia from '@components/user-media'
+import ArrowRight from '@icons/arrow-right'
 
 export default {
   name: 'NewUsers',
